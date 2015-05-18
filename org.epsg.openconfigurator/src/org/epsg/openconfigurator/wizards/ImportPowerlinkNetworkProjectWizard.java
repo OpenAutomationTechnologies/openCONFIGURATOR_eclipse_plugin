f83c14490de5404f483834a2800d3bdeda1e689f
Ramakrishnan P <ramakrishanan_p@kalycito.com>
2015-05-18 22:07:09 +0530

    [FEATURE] Add import openCONFIGURATOR project wizard
    Note: This wizard only imports openCONFIGURATOR projects v1.4.0 and v1.4.1.
       The projects which already has a .project can be imported via
       default eclipse file system/project import mechanism.
    
    Change-Id: I019cf3bc4216c70fdc71fbd65dd8c4a276171841
    Signed-off-by: Ramakrishnan P <ramakrishanan_p@kalycito.com>

---
+++ org.epsg.openconfigurator/src/org/epsg/openconfigurator/wizards/ImportPowerlinkNetworkProjectWizard.java
@@ -0,0 +1,110 @@
+/*******************************************************************************
+ * @file   ImportPowerlinkNetworkProjectWizard.java
+ *
+ * @brief  The main wizard interface for importing POWERLINK network projects.
+ *
+ * @author Ramakrishnan Periyakaruppan, Kalycito Infotech Private Limited.
+ *
+ * @copyright (c) 2015, Kalycito Infotech Private Limited
+ *                    All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the copyright holders nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
+
+package org.epsg.openconfigurator.wizards;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExecutableExtension;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.IImportWizard;
+import org.eclipse.ui.IWorkbench;
+
+//import org.epsg.openconfigurator.wizards.ImportProjectWizardResourceSupport;
+
+/**
+ * @author Ramakrishnan P
+ *
+ */
+public class ImportPowerlinkNetworkProjectWizard extends Wizard implements IImportWizard,
+    IExecutableExtension {
+
+  private ImportOpenConfiguratorProjectWizardPage mainPage;
+
+  /**
+   *
+   */
+  public ImportPowerlinkNetworkProjectWizard() {
+    super();
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see org.eclipse.jface.wizard.Wizard#addPages()
+   */
+  @Override
+  public void addPages() {
+    super.addPages();
+    mainPage = new ImportOpenConfiguratorProjectWizardPage();
+    addPage(mainPage);
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench,
+   * org.eclipse.jface.viewers.IStructuredSelection)
+   */
+  @Override
+  public void init(IWorkbench workbench, IStructuredSelection selection) {
+    // this.setDefaultPageImageDescriptor(IDEWorkbenchPlugin
+    //        .getIDEImageDescriptor("wizban/importproj_wiz.png")); //$NON-NLS-1$
+    IStructuredSelection currentSelection = selection;
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see org.eclipse.jface.wizard.Wizard#performFinish()
+   */
+  @Override
+  public boolean performFinish() {
+    return mainPage.createProjects();
+  }
+
+  /*
+   * (non-Javadoc)
+   *
+   * @see
+   * org.eclipse.core.runtime.IExecutableExtension#setInitializationData(org.eclipse.core.runtime
+   * .IConfigurationElement, java.lang.String, java.lang.Object)
+   */
+  @Override
+  public void setInitializationData(IConfigurationElement config, String propertyName, Object data)
+      throws CoreException {
+    IConfigurationElement _configurationElement = config;
+  }
+
+}
