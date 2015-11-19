c22db7e8752a97a066cf2899dd02b3f4866897b6
Ramakrishnan P <ramakrishanan_p@kalycito.com>
2015-11-19 20:05:06 +0530

    [TASK] Update revision information for V2.0.0
     - Update license information
    
    Change-Id: Ic32b79f5d9a4f354c496690247db79860a7db509
    Signed-off-by: Ramakrishnan P <ramakrishanan_p@kalycito.com>

--- org.epsg.openconfigurator/src/org/epsg/openconfigurator/util/OpenConfiguratorProjectUtils.java
+++ org.epsg.openconfigurator/src/org/epsg/openconfigurator/util/OpenConfiguratorProjectUtils.java
@@ -108,9 +108,9 @@ public final class OpenConfiguratorProjectUtils {
     public static final String AUTO_GENERATION_SETTINGS_NONE_ID = "none"; ////$NON-NLS-1$
     public static final String AUTO_GENERATION_SETTINGS_CUSTOM_ID = "custom"; ////$NON-NLS-1$
 
-    public static final String GENERATOR_VENDOR = "Kalycito Infotech Private Limited &amp; Bernecker + Rainer Industrie Elektronik Ges.m.b.H."; ////$NON-NLS-1$
-    public static final String GENERATOR_TOOL_NAME = "openCONFIGURATOR"; ////$NON-NLS-1$
-    public static final String GENERATOR_TOOL_VERSION = "2.0.0-pre-release"; ////$NON-NLS-1$
+    public static final String GENERATOR_VENDOR = "Kalycito Infotech Private Limited & Bernecker + Rainer Industrie Elektronik Ges.m.b.H."; ////$NON-NLS-1$
+    public static final String GENERATOR_TOOL_NAME = "Ethernet POWERLINK openCONFIGURATOR"; ////$NON-NLS-1$
+    public static final String GENERATOR_TOOL_VERSION = "2.0.0"; ////$NON-NLS-1$
     public static final String SYSTEM_USER_NAME_ID = "user.name";
 
     private static ArrayList<String> defaultBuildConfigurationIdList;
