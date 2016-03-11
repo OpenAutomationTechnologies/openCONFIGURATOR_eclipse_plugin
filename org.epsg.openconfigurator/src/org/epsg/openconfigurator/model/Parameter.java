b22c223bfa81fe3c7dcec9342a19ff2cb822a930
Sree Hari Vignesh <sreeharivignesh.b@kalycito.com>
2016-03-11 19:36:54 +0530

    [TASK] Display values of parameter template in properties
     - Get the property values of parameter template, if parameter does
       not contain those values.
    
    Change-Id: Ib72484790647fdaf9e059194efa1315fc303f2cd
    Signed-off-by: Sree Hari Vignesh <sreeharivignesh.b@kalycito.com>

--- org.epsg.openconfigurator/src/org/epsg/openconfigurator/model/Parameter.java
+++ org.epsg.openconfigurator/src/org/epsg/openconfigurator/model/Parameter.java
@@ -35,18 +35,27 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.epsg.openconfigurator.util.OpenConfiguratorProjectUtils;
 import org.epsg.openconfigurator.xmlbinding.xdd.TParameterList;
+import org.epsg.openconfigurator.xmlbinding.xdd.TParameterTemplate;
 import org.epsg.openconfigurator.xmlbinding.xdd.TProperty;
 import org.jdom2.JDOMException;
 
 /**
+ * Class to list the parameter and define the various parameter elements.
  *
  * @author Ramakrishnan P
  *
  */
 public class Parameter implements IParameter {
 
+    /**
+     * Enum to list out available access option of parameter list.
+     *
+     * @author Ramakrishnan P
+     *
+     */
     public enum ParameterAccess {
         UNDEFINED("undefined"), NO_ACCESS("noAccess"), CONST("const"), READ(
                 "read"), WRITE("write"), READ_WRITE(
@@ -54,6 +63,12 @@ public class Parameter implements IParameter {
                                 "readWriteInput"), READ_WRITE_OUTPUT(
                                         "readWriteOutput");
 
+        /**
+         * Get the values of ParameterAccess enum defined.
+         *
+         * @param v The enum to get the value.
+         * @return The value of given enum as a parameter.
+         */
         public static ParameterAccess fromValue(String v) {
             for (ParameterAccess c : ParameterAccess.values()) {
                 if (c.value.equals(v)) {
@@ -69,21 +84,44 @@ public class Parameter implements IParameter {
             value = text;
         }
 
+        /*
+         * (non-Javadoc)
+         *
+         * @see java.lang.Enum#toString()
+         */
         @Override
         public String toString() {
             return value;
         }
     }
 
+    /**
+     * Property class to list out the properties of parameter.
+     *
+     * @author Ramakrishnan P
+     *
+     */
     public class Property {
         private String name;
         private String value;
 
+        /**
+         * Constructor of Property to define name and value of available
+         * parameter properties.
+         *
+         * @param name The name of parameter property
+         * @param value The value given to update the name into the parameter.
+         */
         public Property(final String name, final String value) {
             this.name = name;
             this.value = value;
         }
 
+        /**
+         * Constructor of Property with XDD model as input.
+         *
+         * @param propModel The XDD model of TProperty.
+         */
         public Property(TProperty propModel) {
             if (propModel != null) {
                 name = propModel.getName();
@@ -93,33 +131,96 @@ public class Parameter implements IParameter {
             }
         }
 
+        /**
+         * @return Name of parameter property.
+         */
         public String getName() {
             return name;
         }
 
+        /**
+         * @return The value of parameter property.
+         */
         public String getValue() {
             return value;
         }
     }
 
-    private TParameterList.Parameter parameter;
+    /**
+     * Instance of Node.
+     */
     private Node node;
+
+    /**
+     * Instance of parameter Xpath.
+     */
     private String xpath;
+
+    /**
+     * Instance of parameter actual value Xpath.
+     */
     private String actualValueXpath;
 
+    /**
+     * Instance of TParameterTemplate XDD model.
+     */
+    private TParameterTemplate parameterTemplate;
+
+    /**
+     * Instance of LabelDescription
+     */
     private LabelDescription label;
+
+    /**
+     * UniqueID of parameter
+     */
     private String uniqueId;
+
+    /**
+     * Instance of parameter access enum.
+     */
     private ParameterAccess access = ParameterAccess.READ;
+
+    /**
+     * Instance of DataTypeChoice.
+     */
     private DataTypeChoice dataType;
+
     // private Parameter conditionalParameter;
     // TODO: ConditionalSupport is ignored as of now.
+
+    /**
+     * Actual value of parameter.
+     */
     private String actualValue;
+
+    /**
+     * Default value of parameter.
+     */
     private String defaultValue;
 
+    /**
+     * Instance of LabelDescription to define the unitLabel element.
+     */
     private LabelDescription unitLabel;
+
+    /**
+     * List of parameter property.
+     */
     private List<Property> propertyList = new ArrayList<>();
+
+    /**
+     * Instance of parameter allowed values.
+     */
     private AllowedValues allowedValues;
 
+    /**
+     * Constructor of parameter class to define values of parameter child
+     * elements.
+     *
+     * @param nodeinstance instance of Node.
+     * @param param Parameter XDD model.
+     */
     public Parameter(Node nodeinstance, TParameterList.Parameter param) {
         if (param != null) {
             label = new LabelDescription(
@@ -129,13 +230,21 @@ public class Parameter implements IParameter {
             if (param.getAccess() != null) {
                 access = ParameterAccess.fromValue(param.getAccess());
             }
-            parameter = param;
+
             node = nodeinstance;
             xpath = "//plk:parameter[@uniqueID='" + uniqueId + "']";
             actualValueXpath = xpath + "/plk:actualValue";
 
             dataType = new DataTypeChoice(param);
 
+            Object paramTempladeIDRef = param.getTemplateIDRef();
+            if (paramTempladeIDRef != null) {
+
+                if (paramTempladeIDRef instanceof TParameterTemplate) {
+                    TParameterTemplate parameteratemplateModel = (TParameterTemplate) paramTempladeIDRef;
+                    parameterTemplate = parameteratemplateModel;
+                }
+            }
             if (param.getActualValue() != null) {
                 actualValue = param.getActualValue().getValue();
             }
@@ -159,41 +268,109 @@ public class Parameter implements IParameter {
         }
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.epsg.openconfigurator.model.IParameter#getAccess()
+     */
     @Override
     public ParameterAccess getAccess() {
+        // TODO:Check for access of templateIDRef
         return access;
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.epsg.openconfigurator.model.IParameter#getActualValue()
+     */
     @Override
     public String getActualValue() {
+        if ((actualValue == null) || (actualValue.isEmpty())) {
+            if (parameterTemplate instanceof TParameterTemplate) {
+                if (parameterTemplate.getActualValue() != null) {
+                    return parameterTemplate.getActualValue().getValue();
+                } else {
+                    return StringUtils.EMPTY;
+                }
+            }
+        }
         return actualValue;
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.epsg.openconfigurator.model.IParameter#getAllowedValues()
+     */
     @Override
     public AllowedValues getAllowedValues() {
+        if ((allowedValues == null)) {
+            if (parameterTemplate instanceof TParameterTemplate) {
+                if (parameterTemplate.getAllowedValues() != null) {
+                    return new AllowedValues(
+                            parameterTemplate.getAllowedValues());
+                } else {
+                    return null;
+                }
+            }
+        }
         return allowedValues;
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.epsg.openconfigurator.model.IParameter#getDataType()
+     */
     @Override
     public DataTypeChoice getDataType() {
         return dataType;
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.epsg.openconfigurator.model.IParameter#getDataTypeChoice()
+     */
     @Override
     public DataTypeChoiceType getDataTypeChoice() {
         return dataType.getChoiceType();
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.epsg.openconfigurator.model.IParameter#getDefaultValue()
+     */
     @Override
     public String getDefaultValue() {
+        if ((defaultValue == null) || (defaultValue.isEmpty())) {
+            if (parameterTemplate instanceof TParameterTemplate) {
+                if (parameterTemplate.getDefaultValue() != null) {
+                    return parameterTemplate.getDefaultValue().getValue();
+                } else {
+                    return StringUtils.EMPTY;
+                }
+            }
+        }
         return defaultValue;
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.epsg.openconfigurator.model.IParameter#getLabelDescription()
+     */
     @Override
     public LabelDescription getLabelDescription() {
+        // TODO:Check for label of templateIDRef
         return label;
     }
 
+    /**
+     * @return The instance of node.
+     */
     public Node getNode() {
         return node;
     }
@@ -205,26 +382,78 @@ public class Parameter implements IParameter {
         return actualValueXpath;
     }
 
+    /**
+     * @return The allowed values of parameter template defined in the
+     *         parameter.
+     */
+    public AllowedValues getParameterTemplateAllowedValues() {
+        if (parameterTemplate.getAllowedValues() != null) {
+            return new AllowedValues(parameterTemplate.getAllowedValues());
+        }
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.epsg.openconfigurator.model.IParameter#getPropertyList()
+     */
     @Override
     public List<Property> getPropertyList() {
         return propertyList;
     }
 
+    /**
+     * @return The rangeList available within the parameter allowed values of
+     *         XDD file.
+     */
+    public List<Range> getRangeList() {
+        if (parameterTemplate != null) {
+            if ((allowedValues.getRangeList() == null)
+                    || (allowedValues.getRangeList().isEmpty())) {
+                if (getParameterTemplateAllowedValues() != null) {
+                    return getParameterTemplateAllowedValues().getRangeList();
+                }
+            }
+        }
+        return allowedValues.getRangeList();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.epsg.openconfigurator.model.IParameter#getSimpleDataType()
+     */
     @Override
     public SimpleDataType getSimpleDataType() {
         return dataType.getSimpleDataType();
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.epsg.openconfigurator.model.IParameter#getStructDataType()
+     */
     @Override
     public StructType getStructDataType() {
         return dataType.getStructDataType();
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.epsg.openconfigurator.model.IParameter#getUniqueId()
+     */
     @Override
     public String getUniqueId() {
         return uniqueId;
     }
 
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.epsg.openconfigurator.model.IParameter#getUnitLabel()
+     */
     @Override
     public LabelDescription getUnitLabel() {
         return unitLabel;
