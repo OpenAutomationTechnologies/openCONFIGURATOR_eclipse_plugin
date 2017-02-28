d0d44fb3cc989aafde9a501b7bfe5e07205de031
Sree Hari Vignesh <sreeharivignesh.b@kalycito.com>
2017-02-28 20:28:27 +0530

    [TASK] Update revision number to V2.1.2
    Change-Id: I25cc9b0952e99f24f9b7fe76827d09e950bec893
    Signed-off-by: Sree Hari Vignesh <sreeharivignesh.b@kalycito.com>

--- org.epsg.openconfigurator/src/org/epsg/openconfigurator/util/OpenConfiguratorProjectUtils.java
+++ org.epsg.openconfigurator/src/org/epsg/openconfigurator/util/OpenConfiguratorProjectUtils.java
@@ -107,7 +107,7 @@ public final class OpenConfiguratorProjectUtils {
 
     public static final String GENERATOR_VENDOR = "Kalycito Infotech Private Limited & Bernecker + Rainer Industrie Elektronik Ges.m.b.H."; ////$NON-NLS-1$
     public static final String GENERATOR_TOOL_NAME = "Ethernet POWERLINK openCONFIGURATOR"; ////$NON-NLS-1$
-    public static final String GENERATOR_TOOL_VERSION = "2.1.1"; ////$NON-NLS-1$
+    public static final String GENERATOR_TOOL_VERSION = "2.1.2"; ////$NON-NLS-1$
     public static final String SYSTEM_USER_NAME_ID = "user.name";
     public static final String MODIFIED_ON_ATTRIBUTE = "modifiedOn";
     public static final String TOOL_VERSION_ATTRIBUTE = "toolVersion";
@@ -246,7 +246,6 @@ public final class OpenConfiguratorProjectUtils {
             node.getProject().refreshLocal(IResource.DEPTH_INFINITE,
                     new NullProgressMonitor());
         } catch (CoreException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         // Updates generator attributes in project file.
